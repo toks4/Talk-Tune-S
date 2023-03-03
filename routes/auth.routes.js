160635c9f131e1ecd4219df697e75903534c70ed
Toks4shima <toks4shima@gmail>
2023-03-03 12:34:02 +0100

    test

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const User = require('../models/User.model')
-
+const bcrypt = require('bcryptjs')
 router.get('/', (req, res, next) => {
     res.json('All good in auth')
 })
