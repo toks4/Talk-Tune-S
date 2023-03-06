ee4cc089bbd1e8a3452c6882992e556c76394668
nominlang <nominsumiya@gmail.com>
2023-03-06 17:05:51 +0100

    commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require("./app");
+require('dotenv').config();
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5173;
